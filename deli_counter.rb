3ce75d3587cb604e6e30a5f104f7e0be5b4febd1
daniel campo <campo_daniel94@yahoo.com>
2020-08-06 00:08:19 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+require "pry"
+
+ def line(katz_deli)
+   if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+     string = "The line is currently:"
+    katz_deli.each_with_index do | customer, index |
+      string << " #{index +1}. #{customer}"
+    end
+    puts string
+  end
+end
+
+def take_a_number (katz_deli,string)
+  katz_deli << string
+  puts "Welcome, #{string}. You are number #{katz_deli.length} in line."
+# binding.pry
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{katz_deli.shift}."
+# binding.pry
+  end
+end

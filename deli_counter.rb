563f045f9c44e6e91cd73e3ed7a19f91fd89c5c4
daniel campo <campo_daniel94@yahoo.com>
2020-08-06 00:07:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,7 @@ def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else 
-    puts "Currently serving #{.shift(katz_deli)}"
+    puts "Currently serving #{katz_deli.shift}"
 # binding.pry
   end
 end
